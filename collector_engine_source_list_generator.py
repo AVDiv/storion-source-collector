b17c082c86afe68ef9daf2dd5df855efbc0cac1e
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2024-12-02 23:18:25 +0530

    Updated request failure bug in the source list generator script

--- collector_engine_source_list_generator.py
+++ collector_engine_source_list_generator.py
@@ -43,18 +43,18 @@ def validate_url(url):
 
 def find_rss_feed(domain):
     potential_feeds = [
-        f"{domain}/rss",
-        f"{domain}/feed",
-        f"{domain}/feeds/posts/default",
-        f"{domain}/rss.xml",
-        f"{domain}/feed.xml",
+        f"https://{domain}/rss",
+        f"https://{domain}/feed",
+        f"https://{domain}/feeds/posts/default",
+        f"https://{domain}/rss.xml",
+        f"https://{domain}/feed.xml",
     ]
     for feed_url in potential_feeds:
         try:
             response = requests.get(feed_url, timeout=5)
             if response.status_code == 200:
                 return feed_url
-        except requests.RequestException:
+        except requests.RequestException as ex:
             continue
     return None
 
@@ -65,7 +65,7 @@ def check_feed_validity(feed_url):
 
 
 def check_robots_txt(domain, article_links):
-    robots_url = f"{domain}/robots.txt"
+    robots_url = f"https://{domain}/robots.txt"
     rp = RobotFileParser()
     rp.set_url(robots_url)
     try:
@@ -79,45 +79,64 @@ def check_robots_txt(domain, article_links):
         return False, 0
 
 
-for index, row in tqdm(
-    sources_df.iterrows(), total=sources_df.shape[0], desc="Processing sources"
-):
-    source_name = row["publisher_name"]
-    country = row["country"]
-    link = str(row["link"])  # Ensure link is a string
-    domain = urlparse(link).netloc if link else ""
+total_source_count = sources_df.shape[0]
+usable_source_count = 0
 
-    is_domain_up = validate_url(link) if domain else False
-    rss_url = find_rss_feed(domain) if is_domain_up else None
-    is_rss_feed_available = rss_url is not None
-    is_rss_feed_valid = check_feed_validity(rss_url) if is_rss_feed_available else False
+with tqdm(
+    total=total_source_count, desc="Processing sources", colour="#903bf8"
+) as pbar:
+    for index, row in sources_df.iterrows():
+        source_name = row["publisher_name"]
+        country = row["country"]
+        link = str(row["link"])  # Ensure link is a string
+        domain = urlparse(link).netloc if link else ""
 
-    article_links = []
-    if is_rss_feed_valid:
-        feed = feedparser.parse(rss_url)
-        article_links = [entry.link for entry in feed.entries]
+        is_domain_up = validate_url(link) if domain else False
+        rss_url = find_rss_feed(domain) if is_domain_up else None
+        is_rss_feed_available = rss_url is not None
+        is_rss_feed_valid = (
+            check_feed_validity(rss_url) if is_rss_feed_available else False
+        )
 
-    is_scraping_allowed, scraping_score = (
-        check_robots_txt(domain, article_links) if article_links else (False, 0)
-    )
-    usable_source = is_domain_up and is_rss_feed_valid and is_scraping_allowed
+        article_links = []
+        if is_rss_feed_valid:
+            feed = feedparser.parse(rss_url)
+            article_links = [entry.link for entry in feed.entries]
 
-    results_df = results_df._append(
-        {
-            "source_name": source_name,
-            "domain": domain,
-            "country": country,
-            "rss_url": rss_url,
-            "usable_source": usable_source,
-            "is_scraping_allowed": is_scraping_allowed,
-            "is_domain_up": is_domain_up,
-            "is_rss_feed_available": is_rss_feed_available,
-            "is_rss_feed_valid": is_rss_feed_valid,
-        },
-        ignore_index=True,
-    )
+        is_scraping_allowed, scraping_score = (
+            check_robots_txt(domain, article_links) if article_links else (False, 0)
+        )
+        usable_source = is_domain_up and is_rss_feed_valid and is_scraping_allowed
 
-    logging.info(f"Processed {source_name} - Usable: {usable_source}")
+        usable_source_count += usable_source
+
+        results_df = results_df._append(
+            {
+                "source_name": source_name,
+                "domain": domain,
+                "country": country,
+                "rss_url": rss_url,
+                "usable_source": usable_source,
+                "is_scraping_allowed": is_scraping_allowed,
+                "is_domain_up": is_domain_up,
+                "is_rss_feed_available": is_rss_feed_available,
+                "is_rss_feed_valid": is_rss_feed_valid,
+            },
+            ignore_index=True,
+        )
+        pbar.write(
+            f"{source_name} - Usable: {'\033[0;32mYes' if usable_source else '\033[0;31mNo'} \033[0m"
+        )
+        pbar.write(
+            f"\t\tDomain Status: {'\033[0;32mUp' if is_domain_up else '\033[0;31mDown' :<6}\033[0m RSS feed availability: {'\033[0;32mYes' if is_rss_feed_available else '\033[0;31mNo' :<3}\033[0m Crawling allowed?: {'\033[0;32mYes' if is_scraping_allowed else '\033[0;31mNo' :<5} \033[0m\n"
+        )
+        pbar.update(1)
+        pbar.set_postfix({"Current": index + 1})
+        logging.info(f"Processed {source_name} - Usable: {usable_source}")
+
+print(f"Total sources: {total_source_count}")
+print(f"Usable sources: {usable_source_count}")
+print(f"Percentage of usable sources: {usable_source_count/total_source_count:.2%}")
 
 # Export results to CSV
 results_df.to_csv("processed_sources.csv", index=False)
